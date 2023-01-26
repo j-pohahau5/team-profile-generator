98225103e5bccdfe6273c3ee6eb3d4358b501d6e
Jonathan Pohahau <j.pohahau@outlook.com>
2023-01-26 15:42:33 -0800

    added main prompt for employees need to give additional prompts for each employee

--- index.js
+++ index.js
@@ -6,5 +6,21 @@ const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
 const addEmployee = () => {
-    
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is employees name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is employees ID number?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is employees email?'
+        },
+    ])
 }
