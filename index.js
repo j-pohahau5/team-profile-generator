4fa30bae55c6a832558f51382fccd83d8de30bf8
Jonathan Pohahau <j.pohahau@outlook.com>
2023-01-26 15:20:36 -0800

    added const requiering the data i need

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+const fs = require ('fs');
+
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
+const addEmployee = () => {
+    
+}
