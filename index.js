8018c898741abd9325a3d4da1601d1504f2a8db9
Jonathan Pohahau <j.pohahau@outlook.com>
2023-01-26 17:57:06 -0800

    added gave functions to push in to empty array

--- index.js
+++ index.js
@@ -1,108 +1,114 @@
-const inquirer = require('inquirer');
-const fs = require ('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const Manager = require('./lib/manager');
-const Engineer = require('./lib/engineer');
-const Intern = require('./lib/intern');
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 
 const teamArr = [];
 const addEmployee = () => {
-    inquirer.prompt([
-        {
-            name: "name",
-            type: "input",
-            message: "What is Manager's name?",
-        },
-        {
-            name: "id",
-            type: "input",
-            message: "What is Manager's ID number?",
-        },
-        {
-            name: "email",
-            type: "input",
-            message: "What is Manager's email?",
-        },
-        {
-            name: "officeNumber",
-            type: "input",
-            message: "What is Manager's office number?",
-        },
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "What is Manager's name?",
+      },
+      {
+        name: "id",
+        type: "input",
+        message: "What is Manager's ID number?",
+      },
+      {
+        name: "email",
+        type: "input",
+        message: "What is Manager's email?",
+      },
+      {
+        name: "officeNumber",
+        type: "input",
+        message: "What is Manager's office number?",
+      },
     ])
-    .then(function({name, id, email, officeNumber}){
-        let manager = new Manager(name, id, email, officeNumber);
-        teamArr.push(manager);
-
-    }) 
+    .then(function ({ name, id, email, officeNumber }) {
+      let manager = new Manager(name, id, email, officeNumber);
+      teamArr.push(manager);
+    })
     .then(
-     inquirer.prompt([
-        {
-          name: "confirmEmployee",
-          type: "list",
-          message: "Please confirm the Employee you'd like to insert info for or that you're done.",
-          choices: [
-              "Engineer",
-              "Intern",
-              "I am done with my team.",
-          ],
-        },
-      ])
-      .then(function (){
-          if("Engineer"){
-              inquirer.prompt([
-                  {
-                      name: "ename",
-                      type: "input",
-                      message: "What is your Engineer's name?",
-                  },
-                  {
-                      name: "eid",
-                      type: "input",
-                      message: "What is your Engineer's ID number?",
-                  },
-                  {
-                      name: "eemail",
-                      type: "input",
-                      message: "What is your Engineer's email?",
-                  },
-                  {
-                      name:"userName",
-                      type: "input",
-                      message: "What is your Engineer's GitHub username"
-                  },
-              ])
-              console.log("Engineer")
-          } else if ("Intern") {
-              inquirer.prompt([
-                  {
-                      name: "iname",
-                      type: "input",
-                      message: "What is your Intern's name?",
-                  },
-                  {
-                      name: "iid",
-                      type: "input",
-                      message: "What is your Intern's ID number?",
-                  },
-                  {
-                      name: "iemail",
-                      type: "input",
-                      message: "What is your Intern's email?",
-                  },
-                  {
-                      name:"school",
-                      type: "input",
-                      message: "What is your Intern's school name?",
-                  },
-              ])
-              console.log("Intern")
-          } 
-      })
-    )
-      
-}
+      inquirer
+        .prompt([
+          {
+            name: "confirmEmployee",
+            type: "list",
+            message:
+              "Please confirm the Employee you'd like to insert info for or that you're done.",
+            choices: ["Engineer", "Intern", "I am done with my team."],
+          },
+        ])
+        .then(function () {
+          if ("Engineer") {
+            inquirer.prompt([
+              {
+                name: "ename",
+                type: "input",
+                message: "What is your Engineer's name?",
+              },
+              {
+                name: "eid",
+                type: "input",
+                message: "What is your Engineer's ID number?",
+              },
+              {
+                name: "eemail",
+                type: "input",
+                message: "What is your Engineer's email?",
+              },
+              {
+                name: "userName",
+                type: "input",
+                message: "What is your Engineer's GitHub username",
+              },
+            ])
+            .then(function ({ ename, eid, eemail, userName}) {
+                let engineer = new Engineer(ename, eid, eemail, userName);
+                teamArr.push(engineer);
+              })
+            console.log("Engineer");
+          }
+           if ("Intern") {
+            inquirer.prompt([
+              {
+                name: "iname",
+                type: "input",
+                message: "What is your Intern's name?",
+              },
+              {
+                name: "iid",
+                type: "input",
+                message: "What is your Intern's ID number?",
+              },
+              {
+                name: "iemail",
+                type: "input",
+                message: "What is your Intern's email?",
+              },
+              {
+                name: "school",
+                type: "input",
+                message: "What is your Intern's school name?",
+              },
+            ])
+            .then(function ({iname, iid, iemail, school}) {
+                let intern = new Intern(iname, iid, iemail, school);
+                teamArr.push(intern);
+              })
+            console.log("Intern");
+          }
+        })
+    );
+};
 
 function init() {
-    addEmployee()
+  addEmployee();
 }
-init()
+init();
