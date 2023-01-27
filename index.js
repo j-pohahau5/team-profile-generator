8b69c8e730fbb5d82913da201ce076d71d8fec77
Jonathan Pohahau <j.pohahau@outlook.com>
2023-01-26 17:29:53 -0800

    added prompt for enigneer and intern, need to fix the prompt way

--- index.js
+++ index.js
@@ -5,22 +5,104 @@ const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
+const teamArr = [];
 const addEmployee = () => {
     inquirer.prompt([
         {
-            type: 'input',
-            name: 'name',
-            message: 'What is employees name?'
+            name: "name",
+            type: "input",
+            message: "What is Manager's name?",
         },
         {
-            type: 'input',
-            name: 'id',
-            message: 'What is employees ID number?'
+            name: "id",
+            type: "input",
+            message: "What is Manager's ID number?",
         },
         {
-            type: 'input',
-            name: 'email',
-            message: 'What is employees email?'
+            name: "email",
+            type: "input",
+            message: "What is Manager's email?",
+        },
+        {
+            name: "officeNumber",
+            type: "input",
+            message: "What is Manager's office number?",
         },
     ])
+    .then(function({name, id, email, officeNumber}){
+        let manager = new Manager(name, id, email, officeNumber);
+        teamArr.push(manager);
+
+    }) 
+    .then(
+     inquirer.prompt([
+        {
+          name: "confirmEmployee",
+          type: "list",
+          message: "Please confirm the Employee you'd like to insert info for or that you're done.",
+          choices: [
+              "Engineer",
+              "Intern",
+              "I am done with my team.",
+          ],
+        },
+      ])
+      .then(function (){
+          if("Engineer"){
+              inquirer.prompt([
+                  {
+                      name: "ename",
+                      type: "input",
+                      message: "What is your Engineer's name?",
+                  },
+                  {
+                      name: "eid",
+                      type: "input",
+                      message: "What is your Engineer's ID number?",
+                  },
+                  {
+                      name: "eemail",
+                      type: "input",
+                      message: "What is your Engineer's email?",
+                  },
+                  {
+                      name:"userName",
+                      type: "input",
+                      message: "What is your Engineer's GitHub username"
+                  },
+              ])
+              console.log("Engineer")
+          } else if ("Intern") {
+              inquirer.prompt([
+                  {
+                      name: "iname",
+                      type: "input",
+                      message: "What is your Intern's name?",
+                  },
+                  {
+                      name: "iid",
+                      type: "input",
+                      message: "What is your Intern's ID number?",
+                  },
+                  {
+                      name: "iemail",
+                      type: "input",
+                      message: "What is your Intern's email?",
+                  },
+                  {
+                      name:"school",
+                      type: "input",
+                      message: "What is your Intern's school name?",
+                  },
+              ])
+              console.log("Intern")
+          } 
+      })
+    )
+      
 }
+
+function init() {
+    addEmployee()
+}
+init()
